344f0764a155065f347bbb2c540b0804f8337d9f
Lam Viet Dinh <dinh.lam@audiencemedia.com>
2016-11-01 19:48:23 +0700

    upload home page

--- truongphat/js/home.js
+++ truongphat/js/home.js
@@ -2,11 +2,8 @@ $(function(){
 	$("#home-info-carousel").owlCarousel({
       loop:true,
       items: 1,
-      nav:false,
-      animateOut: 'slideOutDown',
-      animateIn: 'flipInX',
-      smartSpeed:450
-  	})
+      nav:false  	
+    })
 
   $("#best-customer-carousel").owlCarousel({
     loop:true,
@@ -26,22 +23,58 @@ $(function(){
   })
   $("#truyenthong-carousel").owlCarousel({
     autoplay:true,
-    autoplayTimeout:3000,
+    autoplayTimeout:5000,
     autoplayHoverPause:true,
-    slideSpeed : 300,
-    paginationSpeed : 300,
+    slideSpeed : 500,
+    paginationSpeed : 500,
     loop:true,
     items: 1,
     nav: false
   })
   $("#baochi-carousel").owlCarousel({
     autoplay:true,
-    autoplayTimeout:3000,
+    autoplayTimeout:5000,
     autoplayHoverPause:true,
-    slideSpeed : 300,
-    paginationSpeed : 300,
+    slideSpeed : 500,
+    paginationSpeed : 500,
     loop:true,
     items: 1,
     nav: false
   })
+  function onScrollAnimationInit( items, trigger ) {
+    items.each( function() {
+      var tpElement = $(this),
+          tpAnimationClass = tpElement.attr('data-tp-animation'),
+          tpAnimationDelay = tpElement.attr('data-tp-animation-delay');
+        
+          tpElement.css({
+            '-webkit-animation-delay':  tpAnimationDelay,
+            '-moz-animation-delay':     tpAnimationDelay,
+            'animation-delay':          tpAnimationDelay
+          });
+
+          var tpTrigger = ( trigger ) ? trigger : tpElement;
+          
+          tpTrigger.waypoint(function() {
+            tpElement.addClass('animated').addClass(tpAnimationClass);
+            tpElement.removeAttr('data-tp-animation data-tp-animation-delay style').removeClass('tp-animation');
+            },{
+                triggerOnce: true,
+                offset: '85%'
+          });
+          //tpElement.removeAttr('data-tp-animation data-tp-animation-delay style').removeClass('tp-animation');
+    });
+  }
+  onScrollAnimationInit( $('.tp-animation') );
+  $('#customer').waypoint(function(direction) {
+    if (direction == 'down') {
+      $('#top-nav').addClass('active');
+    }
+    else {
+      $('#top-nav').removeClass('active');
+    }
+     $('#top-nav').toggleClass('active');
+  }, { 
+    offset: '0%' 
+  });
 })
