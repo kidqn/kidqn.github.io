50b696ae684718c6e2d0de7c89e63d29bca7a0f5
dinhlam <lamvietdinhqn2204@gmail.com>
2017-01-12 09:27:34 +0700

    up

--- truongphat/js/baogia.js
+++ truongphat/js/baogia.js
@@ -3,4 +3,12 @@ $(function(){
 	[].slice.call( document.querySelectorAll( 'select.tp-dropdown-select' ) ).forEach( function(el) {	
 		new SelectFx(el);
 	} );
+	$('.tp-popup .close-popup').click(function(){
+		$(this).parent().modal('toggle');
+	})
+});
+$(window).load(function(){
+    setTimeout(function(){ 
+    	$('#baogia-popup').modal('show');
+    }, 3000);
 })
