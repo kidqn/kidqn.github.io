11cf18f8fe0d51251a5e995b0bf72994a5980588
dinhlam <lamvietdinhqn2204@gmail.com>
2017-01-13 10:30:42 +0700

    up

--- truongphat/js/contact.js
+++ truongphat/js/contact.js
@@ -1,31 +1,77 @@
-$(window).load(function(){
-      var center = new google.maps.LatLng(10.783104, 106.684053);
-      function initialize() {
+/*var contactGEO = {};
+function initialize() {
+    contactGEO.uluru = {lat: 10.783098, lng: 106.683978};
+    contactGEO.map = new google.maps.Map(document.getElementById('map-canvas'), {
+        zoom: 15,
+        center: contactGEO.uluru
+    });
+    var marker = new google.maps.Marker({
+        position: contactGEO.uluru,
+        map: contactGEO.map
+    });
+}
+$(function() {
 
-          var mapOptions = {
-              zoom: 15,
-              mapTypeId: google.maps.MapTypeId.ROADMAP,
-              center: center
-          };
+    $('#loc-1').on('click', function () {
+        $('#modal').modal({
+            backdrop: 'static',
+            keyboard: true
+        }).on('shown.bs.modal', function () {
+            google.maps.event.trigger(contactGEO.map, 'resize');
+            contactGEO.map.setCenter(contactGEO.uluru);
+        });
+    });
+});*/
+// global variables
+var map;
+var center;
 
-          map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+function loadScript() {
+    var myKey = "AIzaSyBROYPNa_1AhfRS-GXn5l8j9sb_exQhRhs";
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "http://maps.googleapis.com/maps/api/js?key=" + myKey + "&callback=initialize";
+    document.body.appendChild(script);
+}
+window.onload = loadScript;
 
-          var marker = new google.maps.Marker({
-              map: map,
-              position: center
-          });
-      }
 
-      $('#loc-1').on('click', function () {
-          console.log('sdasd')
-          $('#modal').modal({
-              backdrop: 'static',
-              keyboard: true
-          }).on('shown.bs.modal', function () {
+function initialize() {
+    center = new google.maps.LatLng(59.76522, 18.35002);
+    var mapOptions = {
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: center
+    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+    var marker = new google.maps.Marker({
+        map: map,
+        position: center
+    });
+    marker.setMap(map);
+}
+
+$(function() {
+    $('#loc-1').on('click', function () {
+      //document.getElementById('map-canvas').style.display="block";
+      //initialize();
+      center = new google.maps.LatLng(10.783357, 106.683961);
+        $('#baogia-popup').modal({
+            backdrop: 'static',
+            keyboard: true
+        }).on('shown.bs.modal', function () {
+              var marker = new google.maps.Marker({
+                  map: map,
+                  position: center
+              });
+              marker.setMap(map);
               google.maps.event.trigger(map, 'resize');
               map.setCenter(center);
-          });
-      });
-
-      initialize();
-})
+              
+        });
+    });
+    $('.close-popup').click(function(){
+      $(this).parent().modal('toggle');
+    })
+});
