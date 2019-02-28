bf936e43c648ce68feb7f7601e4a194b9990bd10
bocasay-dinhlam <dlam@bocasay.com>
2019-02-28 12:59:56 +0700

    update

--- nt-decor/js/home.js
+++ nt-decor/js/home.js
@@ -4,9 +4,11 @@ var fixOwl = function(){
         var newWidth =  ( $(this).find('.item').height() / 60.33 ) * 100;
         $(this).find('.item').width(newWidth)
     });
+}
+var refreshOwl = function(){
+    fixOwl();
     $('#home-carousel').trigger('refresh.owl.carousel');
 }
-
 $(function(){
     $('#home-carousel').owlCarousel({
         slideSpeed : 500,
@@ -18,6 +20,6 @@ $(function(){
         dots: false,
         navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
         onInitialized: fixOwl,
-        onResized: fixOwl
-    });
+        onResized: refreshOwl
+    }).trigger('refresh.owl.carousel');
 })
