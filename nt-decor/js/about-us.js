3a626a02f99f6c605f168109d20788d8b0e8f098
dinhlam <lamvietdinhqn2204@gmail.com>
2019-03-04 13:09:35 +0700

    update

--- nt-decor/js/about-us.js
+++ nt-decor/js/about-us.js
@@ -1,10 +1,18 @@
 
-$('#our-team-carousel').owlCarousel({
-  loop: true,
-  dot:true,
-  items: 1,
-  smartSpeed: 1000,
-  autoplay:true,
-  autoplayTimeout:3000,
-  autoplayHoverPause:true
-});
+if($('#mobile-right-nav-menu-btn').is(":visible")) {
+  $( "#our-team-carousel" ).removeClass( "owl-carousel owl-theme" );
+  $("#our-team-carousel").find('.image').each(function(){
+    let tempWidth =  $(this).width();
+    $(this).css('height', tempWidth);
+  })  
+} else {
+  $('#our-team-carousel').owlCarousel({
+    loop: true,
+    dot:true,
+    items: 1,
+    smartSpeed: 1000,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+  });
+}
