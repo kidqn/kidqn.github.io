67ddbeb1ca150820ca97a0b77f4081c067ed0816
bocasay-dinhlam <dlam@bocasay.com>
2019-04-13 10:53:28 +0700

    update

--- nt-decor/js/home.js
+++ nt-decor/js/home.js
@@ -1,4 +1,4 @@
-var fixOwl = function(){
+var fixOwl = function(event){
     var $el = $('.owl-item');
     var newWidth;
     $el.each(function() {
@@ -6,9 +6,12 @@ var fixOwl = function(){
         $(this).find('.item').width(newWidth)
     });
     $('.home-p section#new-ideas > .container > .mask').width(newWidth - 130);
+    var current = event.item.index;
+    var authorData = $(event.target).find('.owl-item').eq(current).find('.info').html();
+    $('#new-ideas #author').html(authorData);
 }
-var refreshOwl = function(){
-    fixOwl();
+var refreshOwl = function(event){
+    fixOwl(event);
     $('#home-carousel').trigger('refresh.owl.carousel');
 }
 $(function(){
@@ -23,5 +26,9 @@ $(function(){
         navText: ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
         onInitialized: fixOwl,
         onResized: refreshOwl
-    }).trigger('refresh.owl.carousel');
+    }).trigger('refresh.owl.carousel').on('changed.owl.carousel', function (e) {
+        var current = e.item.index;
+        var authorData = $(e.target).find('.owl-item').eq(current).find('.info').html();
+        $('#new-ideas #author').html(authorData);
+    })
 })
