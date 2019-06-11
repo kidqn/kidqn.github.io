21e91fc7b97ab3cbe762728da9af710cf2b9f56d
bocasay-dinhlam <dlam@bocasay.com>
2019-06-11 15:18:02 +0700

    update

---
+++ build/precache-manifest.5f105055c4909d12a3f873993b7f93a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edd5ad5b879e5e26a188fdbcebea0ee2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a982981f5e8460bcb7e",
+    "url": "/static/css/main.adf532de.chunk.css"
+  },
+  {
+    "revision": "6a9dcd5a6a1858ccb467",
+    "url": "/static/js/2.cbabd41c.chunk.js"
+  },
+  {
+    "revision": "5a982981f5e8460bcb7e",
+    "url": "/static/js/main.a860805a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
