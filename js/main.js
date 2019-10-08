5f2c84820b63ac9f1755b11c917378481935c8d2
bocasay-dinhlam <dlam@bocasay.com>
2019-10-08 17:48:19 +0700

    update

--- js/main.js
+++ js/main.js
@@ -38,10 +38,31 @@ var logout = function() {
 }
 
 var authByFirebase = function() {
-    console.log(firebase.auth)
-    var credential = firebase.auth.FacebookAuthProvider.credential(accessTokenFB)
-    firebase.auth().signInWithCredential(credential).then((userData) => { 
-        console.log('validate by firebase', userData);
-    })
-    .catch((err) => console.log(err));
+    // console.log(firebase.auth)
+    // var credential = firebase.auth.FacebookAuthProvider.credential(accessTokenFB)
+    // firebase.auth().signInWithCredential(credential).then((userData) => { 
+    //     console.log('validate by firebase', userData);
+    // })
+    // .catch((err) => console.log(err));
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithRedirect(provider);
+    firebase.auth().getRedirectResult().then(function(result) {
+        console.log('data tu firebase', result)
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
 }
