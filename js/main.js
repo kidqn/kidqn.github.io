e1f18c1a3485cf15b02f674135090cedc6ee740d
bocasay-dinhlam <dlam@bocasay.com>
2019-10-08 15:04:06 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC2aT5ZxR4y3P-_141X-CYYrTQvU_K9VW4",
+    authDomain: "react-firebase-26de0.firebaseapp.com",
+    databaseURL: "https://react-firebase-26de0.firebaseio.com",
+    projectId: "react-firebase-26de0",
+    storageBucket: "react-firebase-26de0.appspot.com",
+    messagingSenderId: "589639130157",
+    appId: "1:589639130157:web:21ad3fa3f9c526196caca4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
 var FB;
-var firebase;
 var accessTokenFB = '';
 FB.login(function(response) {
     if (response.authResponse) {
@@ -38,7 +50,7 @@ var logout = function() {
 
 var authByFirebase = function() {
     console.log(firebase)
-    var credential = FacebookAuthProvider.getCredential({
+    var credential = firebase.auth.FacebookAuthProvider.getCredential({
         accessToken: accessTokenFB
     });
     firebase.auth().signInWithCredential(credential).then((userData) => { 
