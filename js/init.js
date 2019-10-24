e4554f0e74b8c0e6cb3088e4e1b047ea8f8dbb3c
bocasay-dinhlam <dlam@bocasay.com>
2019-10-24 11:25:24 +0700

    update

--- js/init.js
+++ js/init.js
@@ -11,6 +11,8 @@
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+
+  const messaging = firebase.messaging();
   messaging.usePublicVapidKey('BJg4xeVdl7UHHckQ9SK7hA2Ko20FWtbtgDkI0Gr2EwbMbMC5ICqr3hxKA7Fc4Yw_FB2OuGZiwBc39WKRw18aWkg');
 
 
