f8519e68b8a7e86a0269749c4e222286ee474d46
bocasay-dinhlam <dlam@bocasay.com>
2019-10-30 17:04:57 +0700

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,38 +1,3 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
-
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({
-    apiKey: "AIzaSyC2aT5ZxR4y3P-_141X-CYYrTQvU_K9VW4",
-    authDomain: "react-firebase-26de0.firebaseapp.com",
-    databaseURL: "https://react-firebase-26de0.firebaseio.com",
-    projectId: "react-firebase-26de0",
-    storageBucket: "react-firebase-26de0.appspot.com",
-    messagingSenderId: "589639130157",
-    appId: "1:589639130157:web:21ad3fa3f9c526196caca4"
-});
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
-
-messaging.setBackgroundMessageHandler((payload) => {
-    console.log('firebase push noti', payload);
-    const title = 'Hello';
-    const options = {
-        body: payload.data.body,
-        icon: '/setting.png',
-        image: '/setting.png',
-        badge: '/setting.png'
-    };
-    console.log('firebase push noti', options);
-    return self.ServiceWorkerRegistration.showNotification(title, options);
-})
-
 self.addEventListener('push', function(event) {  
     var title = 'Yay a message.';
     var body = 'We have received a push message.';
@@ -41,6 +6,7 @@ self.addEventListener('push', function(event) {
   
     event.waitUntil(
         self.registration.pushManager.getSubscription().then(function(subscription) {
+            console.log('subscription', subscription);
             console.info('Received a push message', event.data.json().data);
             self.registration.showNotification(title, {
                 body: body,
