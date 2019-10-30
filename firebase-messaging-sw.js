1c3aca2e65c3f93ff154f2441132bdd60348e75c
bocasay-dinhlam <dlam@bocasay.com>
2019-10-30 15:26:57 +0700

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,3 @@
-
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
@@ -8,9 +7,24 @@ importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': '589639130157'
+    apiKey: "AIzaSyC2aT5ZxR4y3P-_141X-CYYrTQvU_K9VW4",
+    authDomain: "react-firebase-26de0.firebaseapp.com",
+    databaseURL: "https://react-firebase-26de0.firebaseio.com",
+    projectId: "react-firebase-26de0",
+    storageBucket: "react-firebase-26de0.appspot.com",
+    messagingSenderId: "589639130157",
+    appId: "1:589639130157:web:21ad3fa3f9c526196caca4"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler((payload) => {
+    const title = 'Hello';
+    const options = {
+        body: payload.data.status
+    };
+    console.log('firebase push noti', options);
+    return self.ServiceWorkerRegistration.showNotification(title, options);
+})
---
+++ js/init.js
@@ -0,0 +1,38 @@
+var firebase;
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC2aT5ZxR4y3P-_141X-CYYrTQvU_K9VW4",
+  authDomain: "react-firebase-26de0.firebaseapp.com",
+  databaseURL: "https://react-firebase-26de0.firebaseio.com",
+  projectId: "react-firebase-26de0",
+  storageBucket: "react-firebase-26de0.appspot.com",
+  messagingSenderId: "589639130157",
+  appId: "1:589639130157:web:21ad3fa3f9c526196caca4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging()
+messaging.usePublicVapidKey('BLuGEZLnElX-u9TGbAIRE_KWq_Z24Ms5N_BLNEfuVwxK_R5mOgw00O1ARXDcsxbNJk25dYBsfqJbI9NS9RjzVQU');
+
+messaging.requestPermission()
+.then(() => {
+    console.log('Have permissions');
+    return messaging.getToken();
+})
+.then((token) => {
+    console.log(token);
+})
+.catch((err) => console.log('Err', err));
+
+messaging.onTokenRefresh(() => {
+  messaging.getToken().then((refreshedToken) => {
+    console.log('Token refreshed.', refreshedToken);
+  }).catch((err) => {
+    console.log('Unable to retrieve refreshed token ', err);
+  });
+});
+messaging.onMessage((payload) => {
+console.log('Message received. ', payload);
+// ...
+});
