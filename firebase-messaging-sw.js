4de25ea79befc68d7c17a07fae897006b1302574
bocasay-dinhlam <dlam@bocasay.com>
2019-10-31 12:00:53 +0700

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -13,50 +13,39 @@ firebase.initializeApp({
 Retrieve an instance of Firebase Messaging so that it can handle background messages.
 */
 const messaging = firebase.messaging()
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    icon: '/setting.png'
-  };
-  return;
-  // return self.registration.showNotification(notificationTitle,
-  //     notificationOptions);
-});
 
-// self.addEventListener('push', function(event) {
-//     var jsonData = JSON.parse(event.data.text());
-//     console.log('notification data', jsonData);
-//     // jsonData -> here is you data 
-//     const options = {
-//         body: 'set you body',
-//         icon: 'setting.png',
-//         badge: 'setting.png',
-//         sound: 'sharp.mp3'
-//     };
-//     event.waitUntil(self.registration.showNotification(jsonData.data.title, options)); 
-// });
+self.addEventListener('push', function(event) {
+    var jsonData = JSON.parse(event.data.text());
+    console.log('notification data', jsonData);
+    // jsonData -> here is you data 
+    const options = {
+        body: 'set you body',
+        icon: 'setting.png',
+        badge: 'setting.png',
+        sound: 'sharp.mp3'
+    };
+    event.waitUntil(self.registration.showNotification(jsonData.data.title, options)); 
+});
   
-//   self.addEventListener('notificationclick', function(event) {
-//     console.log('On notification click: ', event.notification.tag);
-//     // Android doesn’t close the notification when you click on it
-//     // See: http://crbug.com/463146
-//     event.notification.close();
+  self.addEventListener('notificationclick', function(event) {
+    console.log('On notification click: ', event.notification.tag);
+    // Android doesn’t close the notification when you click on it
+    // See: http://crbug.com/463146
+    event.notification.close();
   
-//     // This looks to see if the current is already open and
-//     // focuses if it is
-//     event.waitUntil(clients.matchAll({
-//       type: 'window'
-//     }).then(function(clientList) {
-//       for (var i = 0; i < clientList.length; i++) {
-//         var client = clientList[i];
-//         if (client.url === '/' && 'focus' in client) {
-//           return client.focus();
-//         }
-//       }
-//       if (clients.openWindow) {
-//         return clients.openWindow('/');
-//       }
-//     }));
-//   });
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(clients.matchAll({
+      type: 'window'
+    }).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if (client.url === '/' && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    }));
+  });
