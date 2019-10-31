0c019f5f1355c2724ab53dffb6d5571b7cb99c01
bocasay-dinhlam <dlam@bocasay.com>
2019-10-31 15:27:56 +0700

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,17 +1,3 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
-
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({
-    messagingSenderId: "589639130157",
-});
-
-const messaging = firebase.messaging();
-
 self.addEventListener('push', function(event) {
     var jsonData = JSON.parse(event.data.text());
     jsonData.data.sender = JSON.parse(jsonData.data.sender);
