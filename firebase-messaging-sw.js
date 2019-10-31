1275c44c07f2e67b005eed87af5c3990a6e9391a
bocasay-dinhlam <dlam@bocasay.com>
2019-10-31 11:33:41 +0700

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -9,7 +9,22 @@ importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
 firebase.initializeApp({
     messagingSenderId: "589639130157",
 });
+/*
+Retrieve an instance of Firebase Messaging so that it can handle background messages.
+*/
+const messaging = firebase.messaging()
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
 
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
 
 self.addEventListener('push', function(event) {
     var jsonData = JSON.parse(event.data.text());
