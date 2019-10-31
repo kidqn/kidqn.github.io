94ac99f791e2f5829ec313c01a7605b76d0d3c82
bocasay-dinhlam <dlam@bocasay.com>
2019-10-31 11:30:38 +0700

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -7,24 +7,9 @@ importScripts('https://www.gstatic.com/firebasejs/7.2.1/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-    messagingSenderId: "75115380018",
+    messagingSenderId: "589639130157",
 });
-/*
-Retrieve an instance of Firebase Messaging so that it can handle background messages.
-*/
-const messaging = firebase.messaging()
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
 
-  return self.registration.showNotification(notificationTitle,
-      notificationOptions);
-});
 
 self.addEventListener('push', function(event) {
     var jsonData = JSON.parse(event.data.text());
